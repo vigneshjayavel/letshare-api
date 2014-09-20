1466cb5dea1c1bd30250ef022862f08b0ba9efb0
karthickpdy <karthickpdy@gmail.com>
2014-09-20 16:24:00 +0530

    models done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ActiveRecord::Base
 	has_many :user_bookings
 	has_many :users, :through => :user_bookings
-	accepts_nested_attributes_for :user_bookings,allow_destroy: true
-	attr_accessible :no_ppl
+	# accepts_nested_attributes_for :user_bookings,allow_destroy: true
+	# attr_accessible :no_ppl
+	
+	
 end
--- app/models/user_booking.rb
+++ app/models/user_booking.rb
@@ -2,4 +2,5 @@ class UserBooking < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :booking
 	attr_accessible :user_id,:booking_id,:no_ppl
+	
 end
