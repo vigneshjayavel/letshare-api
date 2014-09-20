6b18e17390ec2f9b160a53fe217f08d4ae354f2f
Manoj M J <manojmj@freshdesk.com>
2014-09-20 21:27:03 +0530

    fixed join ride

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,15 @@
 class BookingsController < ApplicationController
 	def new
 	end
+
+	CAR_SEATS_MAPPING = {1=>4,2=>5,3=>>7}
 	def create 
 		user=User.find params.delete(:id)
 		no_ppl=params.delete(:no_ppl)
 		booking_val=JSON.parse(params[:bookings])
 		booking_val[:pickup_datetime.to_s]=booking_val[:pickup_datetime.to_s].to_time
 		# params[:bookings][:seats_remaining]=params[:bookings][:seats_remaining]
+		booking_val[:seats_remaining] = CAR_SEATS_MAPPING[booking_val[:vehicle_type]] - no_ppl
 		new_booking = user.bookings.build booking_val
 		new_booking.save!
 		user_booking = user.user_bookings.build
@@ -24,10 +27,13 @@ class BookingsController < ApplicationController
 		user=User.find params[:id]
 		booking_val[:pickup_datetime.to_s]=booking_val[:pickup_datetime.to_s].to_time
 		pickup_datetime_ago = 15.minutes.ago booking_val[:pickup_datetime.to_s]
-		conditions = ["bookings.from = ? AND bookings.to = ? AND (pickup_datetime BETWEEN ? AND ?) AND vehicle_type =? AND users.gender = ?",
+
+		conditions = ["bookings.from = ? AND bookings.to = ? AND (pickup_datetime BETWEEN ? AND ?) AND vehicle_type =? AND users.gender = ? AND seats_remaining >= ?",
 					booking_val[:from.to_s],booking_val[:to.to_s],pickup_datetime_ago,
 					booking_val[:pickup_datetime.to_s],
-					booking_val[:vehicle_type.to_s], user.gender
+					booking_val[:vehicle_type.to_s], 
+					user.gender,
+					params[:no_ppl]
 					]
 		search_result = Booking.find(:all,:joins=>:users,:conditions=>conditions,
 								:order=>'pickup_datetime')
@@ -38,6 +44,24 @@ class BookingsController < ApplicationController
 		end
 		process_result val
 	end
+
+	def join_ride
+		user = User.find(params[:id])
+		no_ppl = params(:no_ppl)
+		booking_val = JSON.parse(params[:booking])
+		booking_id  = booking_val[:id]
+		current_booking=Booking.find(booking_id)
+		seats_remaining_in_this_ride = current_booking.seats_remaining
+		no_ppl = params[:no_ppl]
+		updated_seats = seats_remaining_in_this_ride - no_ppl
+		current_booking.update(:seats_remaining => updated_seats)
+
+		process_result "yes updated"
+
+	end
+
+
+
 	def process_result status = nil
     respond_to do |format|
       format.json {
