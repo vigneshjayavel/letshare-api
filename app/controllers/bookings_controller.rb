f88796172c566a294044aac00ce5baae95d31d3a
karthickpdy <karthickpdy@gmail.com>
2014-09-20 17:00:51 +0530

    contoller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+	def new
+	end
+	def create 
+		user=User.find params.delete(:id)
+		no_ppl=params.delete(:no_ppl)
+		
+		process_result user.bookings 
+	end
+
+	def process_result status = nil
+    respond_to do |format|
+      format.json {
+        render :json => {
+            :status => status
+          }.to_json
+      }
+    	end
+  	end
 end
